260c74434880a73700af58466febbb1bdabec208
Urara08 <90606372+Urara08@users.noreply.github.com>
2022-01-31 21:46:30 +0900

    //

--- src/index.js
+++ src/index.js
@@ -44,3 +44,19 @@
 //cval5[0] = "bird";
 //cval5.push("moneky");
 //cconsole.log(val5);
+
+/**
+ * テンプレート文字列
+ */
+
+const name = "じゃけぇ";
+const age = 28;
+//私の名前はじぇけぇです。年齢は28歳です。
+
+//従来の方法
+const message1 = "私の名前は" + name + "です。年齢は" + age + "です。";
+console.log(message1);
+
+//テンプレート文字列を用いた方法
+const message2 = `私の名前は${name}です。年齢は${age}です。`;
+console.log(message2);
