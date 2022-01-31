d950f5e928924d932d93c96e930abd282ac020f9
Urara08 <90606372+Urara08@users.noreply.github.com>
2022-01-31 21:27:37 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,46 @@
+/**
+ * const. let等の変数宣言
+ */
+//var val1 = "var変数";
+//console.log(val1);
+//console.log();
+
+//var変数は上書き可能
+//val1 = "var変数を上書き";
+//console.log(val1);
+
+//var変数は再宣言可能
+
+//var val1 = "var変数を再宣言";
+// console.log(val1);
+
+//let val2 = "let変数";
+//console.log(val2);
+
+//letは上書き可能
+//val2 = "let変数を上書き";
+//console.log(val2);
+
+//letは再宣言不可
+//llet val2 = "let変数を再宣言";
+
+//const val3 = "const変数";
+//const変数は上書き不可
+//val3 = "const変数を上書き";
+
+//const val3 = "const変数を再宣言";
+//constで定義したプロパティはプロパティの変更が可能
+//const val4 = {
+//  name: "じゃけぇ",
+//  age: 28
+//};
+//console.log(val4);
+
+//val4.name = "jak";
+//val4.address = "Hiroshima";
+//console.log(val4);
+//constで定義した配列はプロパティの変更が可能
+//cconst val5 = ["dog", "cat"];
+//cval5[0] = "bird";
+//cval5.push("moneky");
+//cconsole.log(val5);
