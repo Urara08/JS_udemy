83b4130a0cdec521d1a3130d80197bf61758a75c
Urara08 <90606372+Urara08@users.noreply.github.com>
2022-02-03 02:18:42 +0900

    //

--- src/index.js
+++ src/index.js
@@ -94,11 +94,115 @@
 //です。年齢は${myProfile.age}です。`;
 //console.log(message6);
 
-const myProfile = ["じゃけえ", 28];
+//const myProfile = ["じゃけえ", 28];
 
-const message7 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}です。`;
-console.log(message7);
+//const message7 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}です。`;
+//console.log(message7);
 
-const [name, age] = myProfile;
-const message8 = `名前は${name}です。年齢は${age}です。`;
-console.log(message8);
+//const [name, age] = myProfile;
+//const message8 = `名前は${name}です。年齢は${age}です。`;
+//onsole.log(message8);
+
+/**
+ * デフォルト値、引数など
+ */
+
+//const sayHello = (name = "ゲスト") => console.log(`こんにちは!${name}さん！`);
+//sayHello("じゃけえ");
+
+/**
+ * スプレッド構文...
+ */
+//配列の展開
+//const arr1 = [1, 2];
+//console.log(arr1);
+//console.log(...arr1);
+
+//const sumFunc = (num1, num2) => console.log(num1 + num2);
+//sumFunc(arr1[0], arr1[1]);
+//sumFunc(...arr1);
+
+//まとめる
+//const arr2 = [1, 2, 3, 4, 5];
+//const [num1, num2, ...arr3] = arr2;
+//console.log(num1);
+//console.log(num2);
+//console.log(arr3);
+
+//配列のコピーや、結合
+
+//const arr4 = [10, 20];
+//const arr5 = [30, 40];
+
+//const arr6 = [...arr4];
+//arr6[0] = 100;
+//console.log(arr6);
+//console.log(arr4);
+
+//const arr7 = [...arr4, ...arr5];
+//console.log(arr7);
+
+//const arr8 = arr4;
+//console.log(arr8);
+//arr8[0] = 100;
+//console.log(arr4);
+
+/**
+ * mapや filterを使った配列の処理
+ */
+const nameArr = ["田中", "山田", "じゃけぇ"];
+//for (let index = 0; index < nameArr.length; index++) {
+//  console.log(`${index + 1}番目は${nameArr[index]}です`);
+//}
+//const nameArr2 = nameArr.map((name) => {
+//  return name;
+//});
+//console.log(nameArr2);
+
+//console.log(nameArr2);
+
+//nameArr.map((name, index) => console.log(`${index + 1}番目は${name}です`));
+
+//const numArr = [1, 2, 3, 4, 5];
+//const newNumArr = numArr.filter((num) => {
+//  return num % 2 === 0;
+//});
+//console.log(newNumArr);
+
+//const newNameArr = nameArr.map((name) => {
+// if (name === "じゃけえ") {
+//    return name;
+//  } else {
+//    return `${name}さん`;
+//  }
+//});
+//console.log(newNameArr);
+/**
+ * 三項演算子
+ */
+//ある条件?条件がtrueの時：条件がfolseの時
+//const val1 = 1 < 0 ? `trueです` : `falseです`;
+//console.log(val1);
+
+//const num = 1300;
+
+//const formattedNum =
+//  typeof num === `number` ? num.toLocaleString() : `数値を入力してください。`;
+//console.log(formattedNum);
+
+//const checkSum = (num1, num2) => {
+//  return num1 + num2 > 100 ? `100を越えています!!` : `許容範囲内です`;
+//};
+//console.log(checkSum(50, 40));
+/**
+ * 論理演算子の本当の意味を知ろう
+ */
+//const flag1 = true;
+//const flag2 = false;
+
+//if (flag1 || flag2) {
+//  console.log("1か２はtrueになります");
+//}
+//if (flag1 && flag2) {
+//  console.log("1も２もtrueになります");
+//}
